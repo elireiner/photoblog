4a3dc517af93d13ee651ec8bdafec43cc4a26dec
Eli Reiners <goelyukim@gmail.com>
2020-08-19 19:34:18 -0400

    Add emoji click text

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styles: ['app.component.css']
+})
+
+export class AppComponent {
+    emoji = ['😀', '😃', '😄', '😁' ];
+    activeEmoji: string;
+    changeEmoji() {
+        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)]
+    }
+}
